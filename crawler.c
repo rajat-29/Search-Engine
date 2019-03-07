62c13e36047884908117b08e73288f015b0b5f9a
rajat-29 <rajat295gupta@gmail.com>
2019-03-07 14:13:09 +0530

    second commit

--- crawler.c
+++ crawler.c
@@ -28,7 +28,6 @@ int Check_Depth(char* argv[])  // function to check wheater depth entered in pos
 }
 int Check_Url(char* argv[])  // function to check wheater url is correct or not!!
 {
-    char v[]="wget --spider ";
     char *b = (char*)malloc(sizeof(char)*300);
     strcat(b,"wget --spider ");
     strcat(b,argv[1]);
@@ -44,7 +43,7 @@ int Check_Url(char* argv[])  // function to check wheater url is correct or not!
     }
 }
 
-int Check_Dir(char* argv[])
+int Check_Dir(char* argv[]) // function to check is available or not
 {
     struct stat statbuf;
     if( stat(argv[3],&statbuf) == -1)
@@ -65,6 +64,158 @@ int Check_Dir(char* argv[])
     return 1;
 }
 
+void removeWhiteSpace(char* html)
+{
+  int i;
+  char *buffer = malloc(strlen(html)+1), *p=malloc (sizeof(char)+1);
+  memset(buffer,0,strlen(html)+1);
+  for (i=0;html[i];i++)
+  {
+    if(html[i]>32)
+    {
+      sprintf(p,"%c",html[i]);
+      strcat(buffer,p);
+    }
+  }
+  strcpy(html,buffer);
+  free(buffer); free(p);
+}
+
+
+int GetNextURL(char* html, char* urlofthispage, char* result, int pos)
+{
+  char c;
+  int len, i, j;
+  char* p1;
+  char* p2;
+
+  if(pos == 0) {
+    removeWhiteSpace(html);
+  }
+
+  while (0 != (c = html[pos]))
+  {
+    if ((c=='<') &&
+        ((html[pos+1] == 'a') || (html[pos+1] == 'A'))) {
+      break;
+    }
+    pos++;
+  }
+
+  if (c)
+  {
+    p1 = strchr(&(html[pos+1]), '=');
+
+    if ((!p1) || (*(p1-1) == 'e') || ((p1 - html - pos) > 10))
+    {
+      return GetNextURL(html,urlofthispage,result,pos+1);
+    }
+    if (*(p1+1) == '\"' || *(p1+1) == '\'')
+      p1++;
+
+    p1++;
+
+    p2 = strpbrk(p1, "\'\">");
+    if (!p2)
+    {
+      return GetNextURL(html,urlofthispage,result,pos+1);
+    }
+    if (*p1 == '#')
+    {
+      return GetNextURL(html,urlofthispage,result,pos+1);
+    }
+    if (!strncmp(p1, "mailto:",7))
+      return GetNextURL(html, urlofthispage, result, pos+1);
+    if (!strncmp(p1, "http", 4) || !strncmp(p1, "HTTP", 4))
+    {
+      strncpy(result, p1, (p2-p1));
+      return  (int)(p2 - html + 1);
+    } else {
+      if (p1[0] == '.') {
+        return GetNextURL(html,urlofthispage,result,pos+1);
+      }
+      if (p1[0] == '/') {
+        for (i = 7; i < strlen(urlofthispage); i++)
+          if (urlofthispage[i] == '/')
+            break;
+        strcpy(result, urlofthispage);
+        result[i] = 0;
+        strncat(result, p1, (p2 - p1));
+        return (int)(p2 - html + 1);
+      } else {
+        len = strlen(urlofthispage);
+        for (i = (len - 1); i >= 0; i--)
+          if (urlofthispage[i] == '/')
+            break;
+        for (j = (len - 1); j >= 0; j--)
+          if (urlofthispage[j] == '.')
+              break;
+        if (i == (len -1)) {
+            strcpy(result, urlofthispage);
+            result[i + 1] = 0;
+            strncat(result, p1, p2 - p1);
+            return (int)(p2 - html + 1);
+        }
+        if ((i <= 6)||(i > j)) {
+          strcpy(result, urlofthispage);
+          result[len] = '/';
+          strncat(result, p1, p2 - p1);
+          return (int)(p2 - html + 1);
+        }
+        strcpy(result, urlofthispage);
+        result[i + 1] = 0;
+        strncat(result, p1, p2 - p1);
+        return (int)(p2 - html + 1);
+      }
+    }
+  }
+  return -1;
+}
+
+char* convertData()
+{
+	struct stat st;
+	stat("temp.txt", &st);
+	int File_Size = st.st_size, i=0;
+	char *File_Content = (char*)malloc(sizeof(char) * File_Size);
+	char ch;
+	FILE *file = fopen("temp.txt", "r");
+	ch = getc(file);
+	while(ch != EOF)
+	{
+	  File_Content[i] = ch;
+	  ch = getc(file);
+	  i++;
+	}
+	 File_Content[i] = '\0';
+	 fclose(file);
+	return File_Content;
+}
+
+
+char Transfer_File()  // function to shift data from temp file to new actual file
+{
+	char File_Name[10],ch;
+	static int file_no=1;
+	sprintf(File_Name,"%d",file_no);
+	strcat(File_Name, ".txt");
+        FILE *oldFileName = fopen("temp.txt" , "r");
+	FILE *newFileName = fopen(File_Name, "w");
+	ch = getc(oldFileName);
+	while(ch != EOF)
+	{
+	   putc(ch, newFileName);
+	   ch = getc(oldFileName);
+	}
+	file_no++;
+	printf("New File Created");
+	fclose(oldFileName);
+	fclose(newFileName);
+}
+
+
+
+
 void get_Page(char *url)  // function to fetch url from user and contact in urlBuffer and fetch page source code and add it to temp file
 {
     char urlBuffer[urlLength + 300] = {0};
@@ -72,7 +223,10 @@ void get_Page(char *url)  // function to fetch url from user and contact in urlB
     strcat(urlBuffer, "./temp.txt ");
     strcat(urlBuffer, url);
     system(urlBuffer);
+    Transfer_File();
 }
+
+
 
 void Check_Arguments(int argc,char* argv[])  // function to check whether all arguments are correct or not!!
 {
@@ -95,4 +249,21 @@ int main(int argc,char* argv[])
 {
     Check_Arguments(argc,argv);
     get_Page(argv[1]);
+
+    struct stat st;
+    stat("temp.txt", &st);
+    int File_Size = st.st_size;
+File_Size++;
+
+char *data = (char*)malloc(File_Size*sizeof(char));
+data = convertData();
+
+char *result = (char*)malloc(File_Size*sizeof(char));
+int ans = GetNextURL(data,argv[1],result,0);
+for(int i=0;i<20;i++)
+{
+printf("\n%s",result);
+ans = GetNextURL(data,argv[1],result,ans);
+}
+    
 }
